65a81b73c736b6019ae1cbaac9e6cbdee8240a09
hydrogen602 <hydrogen31415@gmail.com>
2024-02-21 23:55:10 -0600

    Documentation

--- src/error.rs
+++ src/error.rs
@@ -3,20 +3,32 @@ use thiserror_no_std as thiserror;
 
 #[derive(thiserror::Error, Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
+/// Errors that can occur when setting up the soil sensor, or when trying to read from it
 pub enum SetupError {
+    /// Something went wrong with I2C
     #[error("I2C error: {0}")]
     I2C(#[from] I2CError),
+    /// The sensor returned an invalid hardware ID.
+    /// This suggests that the sensor is not connected, is not functioning correctly, or is not the Stemma Soil Sensor.
     #[error("Invalid device: {hw_id}")]
     InvalidDevice { hw_id: u8 },
+    /// The I2C address is invalid. The Stemma Soil Sensor can only be configured to use
+    /// one of four addresses: 0x36, 0x37, 0x38, or 0x39.
+    /// See the [Adafruit documentation](https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor/pinouts)
+    #[error("Invalid I2C address: {address}. The sensor address must be between 0x36 and 0x39, inclusive.")]
+    InvalidI2CAddress { address: u8 },
 }
 
 #[derive(thiserror::Error, Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[error("I2C error: {0:?}")]
+/// Errors that can occur when trying to read from or write to the I2C bus
 pub struct I2CError(pub i2c::AbortReason);
 
+/// A type alias for the result of an I2C operation
 pub type I2CResult<T> = core::result::Result<T, I2CError>;
 
+/// A type alias for the result of setting up the soil sensor
 pub type SetupResult<T> = core::result::Result<T, SetupError>;
 
 impl From<i2c::Error> for I2CError {
@@ -48,6 +60,7 @@ impl From<i2c::Error> for I2CError {
 }
 
 #[cfg(feature = "picoserve")]
+/// This allows returning an I2CError as a response to an HTTP request
 impl picoserve::response::IntoResponse for I2CError {
     async fn write_to<
         R: picoserve::io::Read,
@@ -70,6 +83,7 @@ impl picoserve::response::IntoResponse for I2CError {
 }
 
 #[cfg(feature = "picoserve")]
+/// This allows returning a SetupError as a response to an HTTP request
 impl picoserve::response::IntoResponse for SetupError {
     async fn write_to<
         R: picoserve::io::Read,
--- src/soil_sensor.rs
+++ src/soil_sensor.rs
@@ -13,26 +13,47 @@ const HW_ID_DELAY: u64 = 125;
 
 const TEMP_C_CONSTANT: f32 = 0.00001525878;
 
+/// Represents the Stemma Soil Sensor
 pub struct SoilSensor<'i> {
     i2c: i2c::I2c<'i, I2C0, i2c::Blocking>,
     address: u8,
 }
 
 impl SoilSensor<'_> {
+    /// Create a new soil sensor with the default address of 0x36
     pub async fn new(sda: PIN_4, scl: PIN_5, i2c: I2C0) -> SetupResult<Self> {
+        Self::new_with_address(sda, scl, i2c, 0x36).await
+    }
+
+    /// Create a new soil sensor with a specific address
+    pub async fn new_with_address(
+        sda: PIN_4,
+        scl: PIN_5,
+        i2c: I2C0,
+        address: u8,
+    ) -> SetupResult<Self> {
+        if address < 0x36 || address > 0x39 {
+            return Err(SetupError::InvalidI2CAddress { address });
+        }
+
         let i2c: i2c::I2c<'_, I2C0, i2c::Blocking> =
             i2c::I2c::new_blocking(i2c, scl, sda, i2c::Config::default());
 
-        let mut sensor = SoilSensor { i2c, address: 0x36 };
+        let mut sensor = SoilSensor { i2c, address };
         let hw_id = sensor.read_hw_id().await?;
 
         if hw_id == seesaw::SENSOR_HW_ID_CODE {
+            #[cfg(feature = "defmt")]
+            defmt::info!("Soil sensor connected with address 0x{:02X}", address);
             Ok(sensor)
         } else {
             Err(SetupError::InvalidDevice { hw_id })
         }
     }
 
+    /// Read the temperature from the sensor, returned in Celsius.
+    /// It's not high precision, maybe good to + or - 2 degrees Celsius.
+    /// See the [Adafruit documentation](https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor/overview)
     pub async fn read_temperature(&mut self) -> I2CResult<i32> {
         let mut buf = [0; 4];
         self.i2c_read(
@@ -45,10 +66,12 @@ impl SoilSensor<'_> {
 
         // rounding to the nearest integer (+1/2, then cast)
         // the sensor is maybe good to + or - 2 degrees Celsius, so the decimal places are meaningless
-        // https://www.adafruit.com/product/4026
         Ok((raw as f32 * TEMP_C_CONSTANT + 0.5) as i32)
     }
 
+    /// Read the moisture from the sensor.
+    /// The reading ranges from about 200 (very dry) to 2000 (very wet).
+    /// See the [Adafruit documentation](https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor/overview)
     pub async fn read_moisture(&mut self) -> I2CResult<u16> {
         let mut buf = [0; 2];
 
@@ -62,6 +85,9 @@ impl SoilSensor<'_> {
         Ok(u16::from_be_bytes(buf))
     }
 
+    /// Read the hardware ID from the sensor.
+    /// This should always be 0x55 for the Stemma Soil Sensor.
+    /// This is useful for checking that the device is indeed the soil sensor.
     pub async fn read_hw_id(&mut self) -> I2CResult<u8> {
         let mut buf = [0; 1];
         self.i2c_read(
@@ -74,6 +100,7 @@ impl SoilSensor<'_> {
         Ok(buf[0])
     }
 
+    /// Reads a value from the seesaw chip over I2C.
     async fn i2c_read(&mut self, bytes: &[u8], buffer: &mut [u8], delay_us: u64) -> I2CResult<()> {
         self.i2c.write(self.address, bytes)?;
         Timer::after_micros(delay_us).await;
@@ -82,7 +109,11 @@ impl SoilSensor<'_> {
     }
 }
 
+/// Constants needed for I2C communication
+/// with the seesaw chip.
+/// See https://github.com/adafruit/Adafruit_CircuitPython_seesaw/ for details
 mod seesaw {
+    /// `read_hw_id()` should return this value
     pub const SENSOR_HW_ID_CODE: u8 = 0x55;
 
     pub const STATUS_BASE: u8 = 0x00;
